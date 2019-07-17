d0743e14c5a5d75836d42602620a484b61a9febc
hatashogo <s1711392@coins.tsukuba.ac.jp>
2019-07-18 08:54:41 +0900

    [add]料金の支払い機能と使用量の追加
    水道の使用量の登録と料金の支払いの機能を追加しました。
    支払い料金の計算では、水道料金+先月の繰越分等の料金を計算する予定の関数です。
    料金の支払いは、直接アドレスのetherを引き落とす方法がわからなかったので、一度フロントエンド側に支払い金額を返して、それを元に支払い金額をコントラクトに送金することを想定しています。もっと良い方法が思い付いたら書き換えてしまってください。

--- Waterworks.sol
+++ Waterworks.sol
@@ -3,19 +3,20 @@ pragma solidity 0.5.10;
 contract Waterworks {
     
     address public owner;
-    uint256 public collectAmount;
+    uint256 public collected_money;//集金の総額
     uint256 public basic_charge;
     uint256 public exceedance_money;
     mapping (address => MemberStatus) public member;
     
     struct MemberStatus {
-        uint256 start_month;
-        bool is_supplied;
-        uint256 amount_of_water;
+        uint256 start_month;//使用開始月
+        bool is_supplied;//使用中であるかどうか
+        uint256 amount_of_water;//当月の使用量
         uint256 carryover_money;
-        uint256 carryover_counter;
+        uint256 not_pay_counter;//未払い回数
+        uint256 payment_amount;//支払い金額の合計
         bool is_not_paid;
-        uint256[] history;
+        uint256[] history;//使用量の履歴
     }
     
     modifier onlyOwner(){
@@ -33,21 +34,36 @@ contract Waterworks {
         member[_useraddress].is_supplied = true;
     }
     
+    //支払い料金の計算
+    function calcPaymentAmount() public returns (uint) {
+        member[msg.sender].payment_amount = calcCharge();
+        return  member[msg.sender].payment_amount;
+    }
+    
     //水道料金の計算
-    function calcCharge() public returns (uint) {
-        uint256 charge;
+    function calcCharge() public returns (uint charge) {
         charge = (basic_charge + calcCommodityCharge(member[msg.sender].amount_of_water)) * 108 / 100;
         return charge;
     }
     
     //従量料金の計算
-    function calcCommodityCharge(uint _amount_of_water) public returns (uint){
+    function calcCommodityCharge(uint _amount_of_water) public returns (uint) {
         return (_amount_of_water - 10) * exceedance_money;
     }
     
     //水道料金の支払い
-    function payment() {
-        
+    function payment() public payable {
+        if(member[msg.sender].payment_amount != msg.value) revert();
+        collected_money += msg.value;
+        member[msg.sender].not_pay_counter = 0;
+    }
+    
+    //使用水量の登録
+    function setAmountOfWater(uint _amount_of_water) public returns (uint){
+        member[msg.sender].amount_of_water = _amount_of_water;
+        member[msg.sender].history.push(_amount_of_water);
+        member[msg.sender].not_pay_counter++;
+        return calcPaymentAmount();
     }
     
     //履歴を返す
@@ -60,17 +76,10 @@ contract Waterworks {
     
     //水道を止める
     function stopSupply() public onlyOwner {
-        if (member[msg.sender].carryover_counter >= 3) {
+        if (member[msg.sender].not_pay_counter >= 3) {
             member[msg.sender].is_supplied = false;
         }
     }
 }
-
-
-
-
-
-
-
 
 
